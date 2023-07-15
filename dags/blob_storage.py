1a5aed4b6d297526782303a6b12163beb633352a
Edward Nguyen <68344387+dean6969@users.noreply.github.com>
2023-07-16 02:54:13 +1000

    Update blob_storage.py

--- dags/blob_storage.py
+++ dags/blob_storage.py
@@ -7,7 +7,7 @@ from delete_source_file import delete_source_file
 from data_process_uber import transform_data
 
 default_args = {
-    'start_date': datetime(2023, 1, 1),
+    'start_date': datetime(2023, 07, 16),
 }
 
 dag = DAG(
